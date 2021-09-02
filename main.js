6cbec733c53e250384e781f44c6354ad814bc3ae
“Sammi <naturalbeauty801@gmail.com>
2021-09-02 16:46:35 -0400

    javascript is grabbing values

--- main.js
+++ main.js
@@ -6,15 +6,31 @@ slider.oninput = function() {
   output.innerHTML = this.value;
 }
 
-const initialBill = parseInt(document.getElementById("initialBillAmount").value);
-const numOfPeople = document.getElementById("totalPeople").value;
-const tipPercentage = document.getElementById("tipPercentage").value
+// document.getElementById('initialBillAmount').value.onchange = function(){
+//     console.log(value)
+// }
 
-const tenPercent = (initialBill * 0.1)
+let calculateTip = (() => {
+    let billAmount = document.getElementById('initialBillAmount').value
+    let amountOfPeople = slider.value
+    let tipPercentage = document.getElementById('tipPercent').value
 
-const tipTotal = (tenPercent / 2) + tenPercent
+    // Thought this would take tip percentage value and do math on click 
+    // if(tipPercentage === .1){
+    //     return billAmount *= .1
+    // }else if(tipPercentage === 0.15){
+    //     return billAmount *= .15
+    // }else if(tipPercentage === 0.20){
+    //     return billAmount *= .20
+    // } else if(tipPercentage === 0.25){
+    //     return billAmount *= .20
+    // }else{
+    //     return "error"
+    // }
 
-const tipPerPerson = (tipTotal/ numOfPeople)
-
+    console.log(billAmount,amountOfPeople,tipPercentage)
+          
+})
+// console.log(calculateTip())
 
 
